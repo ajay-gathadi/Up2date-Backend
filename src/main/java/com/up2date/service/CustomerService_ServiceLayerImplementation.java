946583f215711abe15e94b9e6cb5502121d7db04
Ajay Gathadi <ajaygathadi@gmail.com>
2025-05-18 11:33:21 +0530

    Add CustomerService_ServiceLayerImplementation to move business logic to the service layer following the Single Responsibility Principle

---
+++ src/main/java/com/up2date/service/CustomerService_ServiceLayerImplementation.java
@@ -0,0 +1,54 @@
+package com.up2date.service;
+
+import com.up2date.dto.CustomerServiceDTO;
+import com.up2date.entity.Customer;
+import com.up2date.entity.CustomerService;
+import com.up2date.entity.Employee;
+import com.up2date.repository.CustomerRepository;
+import com.up2date.repository.CustomerServiceRepository;
+import com.up2date.repository.EmployeeRepository;
+import com.up2date.repository.ServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+class CustomerService_ServiceLayerImplementation implements CustomerService_ServiceLayer {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private ServiceRepository serviceRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private CustomerServiceRepository customerServiceRepository;
+
+    @Override
+    public CustomerService createCustomerService(CustomerServiceDTO customerServiceDTO){
+        CustomerService customerService = new CustomerService();
+
+        Customer customer = customerRepository.findById(customerServiceDTO.getCustomerId())
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
+
+        com.up2date.entity.Service service = serviceRepository.findById(customerServiceDTO.getServiceId())
+                .orElseThrow(() -> new ResourceNotFoundException("Service not found"));
+
+        Employee employee = null;
+        if(customerServiceDTO.getEmployeeId() != null){
+            employee = employeeRepository.findById(customerServiceDTO.getEmployeeId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
+        }
+
+        customerService.setCustomer(customer);
+        customerService.setService(service);
+        customerService.setCSEmployeeId(employee);
+        customerService.setServiceTakenDate(customerServiceDTO.getServiceTakenDate());
+        customerService.setPaymentMethod(customerServiceDTO.getPaymentMethod());
+        customerService.setAmount(customerService.getAmount());
+
+        return customerServiceRepository.save(customerService);
+    }
+}
