deb2da66ad786317f3d8a15eafcdad54add5dc9f
Ajay Gathadi <ajaygathadi@gmail.com>
2025-06-11 22:14:16 +0530

    Introduce a new method to show the most used services

--- src/main/java/com/up2date/repository/ServiceRepository.java
+++ src/main/java/com/up2date/repository/ServiceRepository.java
@@ -2,13 +2,18 @@ package com.up2date.repository;
 
 import com.up2date.entity.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ServiceRepository extends JpaRepository<Service, Long> {
     // This interface is responsible for interacting with the database for service-related operations.
     // It will contain methods to perform CRUD operations on the services table.
-    // For example, you can use Spring Data JPA to create a repository interface like this:
     List<Service> findByForMaleIsTrue();
     List<Service> findByForFemaleIsTrue();
+
+    @Query("SELECT s FROM Service s LEFT JOIN s.customerServices cs GROUP BY s.serviceId ORDER BY COUNT(cs) DESC")
+    List<Service> findAllOrderByServiceUsageCount();
 }
