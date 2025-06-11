5c1d61f9c050bb6a77548a06b11920137244f7c5
Ajay Gathadi <ajaygathadi@gmail.com>
2025-06-11 22:17:33 +0530

    Calculate the total business for a particular date

--- src/main/java/com/up2date/repository/CustomerServiceRepository.java
+++ src/main/java/com/up2date/repository/CustomerServiceRepository.java
@@ -2,6 +2,10 @@ package com.up2date.repository;
 
 import com.up2date.entity.CustomerService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerServiceRepository extends JpaRepository<CustomerService, Long> {
+    @Query(value = "SELECT COALESCE(SUM(amount), 0) FROM customer_service  " +
+            "WHERE DATE(service_taken_date) = CURRENT_DATE", nativeQuery = true)
+    Double getTotalBusinessForDate();
 }
