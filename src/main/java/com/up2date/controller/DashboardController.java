1dd9ca77e2d10bdec39d6fda19a0de8280422741
Ajay Gathadi <ajaygathadi@gmail.com>
2025-06-11 22:17:58 +0530

    Controller to show data about business

---
+++ src/main/java/com/up2date/controller/DashboardController.java
@@ -0,0 +1,30 @@
+package com.up2date.controller;
+
+import com.up2date.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
+public class DashboardController {
+    private final DashboardService dashboardService;
+
+    @Autowired
+    public DashboardController(DashboardService dashboardService){
+        this.dashboardService = dashboardService;
+    }
+
+    @GetMapping("/total-business-forDate")
+    public ResponseEntity<Double> getTotalBusinessForDate(){
+        try{
+            double totalBusiness = dashboardService.calculateTotalBusinessForDate();
+            return ResponseEntity.ok(totalBusiness);
+        } catch (Exception e){
+            System.err.println("Error calculating total business for today: " + e.getMessage());
+            return  ResponseEntity.internalServerError().body(0.0);
+        }
+    }
+}
