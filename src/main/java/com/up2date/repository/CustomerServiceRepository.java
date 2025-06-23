99441480c4af636be6e946de5f1f658a0a704102
Ajay Gathadi <ajaygathadi@gmail.com>
2025-06-23 21:28:20 +0530

    Add LocalDate for date wise filtering of the data

--- src/main/java/com/up2date/repository/CustomerServiceRepository.java
+++ src/main/java/com/up2date/repository/CustomerServiceRepository.java
@@ -3,17 +3,20 @@ package com.up2date.repository;
 import com.up2date.entity.CustomerService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface CustomerServiceRepository extends JpaRepository<CustomerService, Long> {
     @Query(value = "SELECT COALESCE(SUM(amount), 0) FROM customer_service  " +
-            "WHERE DATE(service_taken_date) = CURRENT_DATE", nativeQuery = true)
-    Double getTotalBusinessForDate();
+            "WHERE DATE(service_taken_date) = :date", nativeQuery = true)
+    Double getTotalBusinessForDate(@Param("date")LocalDate date);
 
     @Query(value = "SELECT COALESCE(SUM(cash_amount),0) FROM customer_service "+
-    "WHERE DATE(service_taken_date) = CURRENT_DATE", nativeQuery = true)
-    Double getTotalCashCollectedForDate();
+    "WHERE DATE(service_taken_date) = :date", nativeQuery = true)
+    Double getTotalCashCollectedForDate(@Param("date") LocalDate date);
 
     @Query(value = "SELECT COALESCE(SUM(online_amount),0) FROM customer_service "+
-    "WHERE DATE(service_taken_date) = CURRENT_DATE", nativeQuery = true)
-    Double getTotalOnlineCollectedForDate();
+    "WHERE DATE(service_taken_date) = :date", nativeQuery = true)
+    Double getTotalOnlineCollectedForDate(@Param("date") LocalDate date);
 }
