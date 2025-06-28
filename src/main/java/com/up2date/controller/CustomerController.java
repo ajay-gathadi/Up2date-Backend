8b069c987001d0befe92a1dd16ebdbcc1736cc8a
Ajay Gathadi <ajaygathadi@gmail.com>
2025-06-28 15:31:22 +0530

    Remove customer creation which was creating new entry in the database for services taken, creating discrepancy in the data

--- src/main/java/com/up2date/controller/CustomerController.java
+++ src/main/java/com/up2date/controller/CustomerController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @RestController
@@ -36,33 +37,71 @@ public class CustomerController {
     @PostMapping("/create")
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
         try{
-            Set<CustomerService> customerServices = new HashSet<>();
-
-            if(customer.getServicesTaken() != null){
-                for(CustomerService customerService : customer.getServicesTaken()){
-                    Service service = customerService.getService();
-                    if(service != null && service.getServiceId() != null){
-                        serviceRepository.findById(service.getServiceId()).ifPresent(validServices -> {
-                            CustomerService newCustomerService = new CustomerService();
-                            newCustomerService.setCustomer(customer);
-                            newCustomerService.setService(validServices);
-                            newCustomerService.setServiceTakenDate(LocalDateTime.now());
-                            newCustomerService.setAmount(customerService.getAmount());
-                            newCustomerService.setPaymentMethod(customerService.getPaymentMethod());
-                            newCustomerService.setCashAmount(customerService.getCashAmount());
-                            newCustomerService.setOnlineAmount(customerService.getOnlineAmount());
-
-                            if(customerService.getCSEmployeeId() != null){
-                                newCustomerService.setCSEmployeeId(customerService.getCSEmployeeId());
-                            }
-                            customerServices.add(newCustomerService);
-                        });
-                    }
-                }
-            }
-            customer.setServicesTaken(customerServices);
+//            Set<CustomerService> customerServices = new HashSet<>();
+//
+//            if(customer.getServicesTaken() != null && !customer.getServicesTaken().isEmpty()){
+//
+//                CustomerService firstService = customer.getServicesTaken().iterator().next();
+//                double totalAmount = firstService.getAmount();
+//                String paymentMethod = firstService.getPaymentMethod();
+//                double cashAmount = firstService.getCashAmount();
+//                double onlineAmount = firstService.getOnlineAmount();
+//                boolean isFirstService = true;
+//
+//                for(CustomerService customerService : customer.getServicesTaken()){
+//                    Optional<Service> optionalService = serviceRepository.findById(customerService.getService().getServiceId());
+//
+//                    if(optionalService.isPresent()){
+//                        Service service = optionalService.get();
+//                        CustomerService newCustomerService = new CustomerService();
+//                        newCustomerService.setCustomer(customer);
+//                        newCustomerService.setService(service);
+//                        newCustomerService.setServiceTakenDate(LocalDateTime.now());
+//
+//                        if(isFirstService){
+//                            newCustomerService.setAmount(totalAmount);
+//                            newCustomerService.setPaymentMethod(paymentMethod);
+//                            newCustomerService.setCashAmount(cashAmount);
+//                            newCustomerService.setOnlineAmount(onlineAmount);
+//                            isFirstService = false;
+//                        } else {
+//                            newCustomerService.setAmount(0.0);
+//                            newCustomerService.setPaymentMethod(paymentMethod);
+//                            newCustomerService.setCashAmount(0.0);
+//                            newCustomerService.setOnlineAmount(0.0);
+//                        }
+//
+//                        if(customerService.getCSEmployeeId() != null){
+//                            newCustomerService.setCSEmployeeId(customerService.getCSEmployeeId());
+//                        }
+//                        customerServices.add(newCustomerService);
+//                    }
+//                    /*
+//                    Service service = customerService.getService();
+//                    if(service != null && service.getServiceId() != null){
+//                        serviceRepository.findById(service.getServiceId()).ifPresent(validServices -> {
+//                            CustomerService newCustomerService = new CustomerService();
+//                            newCustomerService.setCustomer(customer);
+//                            newCustomerService.setService(validServices);
+//                            newCustomerService.setServiceTakenDate(LocalDateTime.now());
+//                            newCustomerService.setAmount(customerService.getAmount());
+//                            newCustomerService.setPaymentMethod(customerService.getPaymentMethod());
+//                            newCustomerService.setCashAmount(customerService.getCashAmount());
+//                            newCustomerService.setOnlineAmount(customerService.getOnlineAmount());
+//
+//                            if(customerService.getCSEmployeeId() != null){
+//                                newCustomerService.setCSEmployeeId(customerService.getCSEmployeeId());
+//                            }
+//                            customerServices.add(newCustomerService);
+//                        });
+//                    }
+//                    */
+//                }
+//            }
+//            customer.setServicesTaken(customerServices);
+//            Customer savedCustomer = customerRepository.save(customer);
+//            System.out.println("Received Customer is : " + customer);
             Customer savedCustomer = customerRepository.save(customer);
-            System.out.println("Received Customer is : " + customer);
             return ResponseEntity.ok(savedCustomer);
         }catch (Exception e){
             System.err.println("Error saving customer: " + e.getMessage());
