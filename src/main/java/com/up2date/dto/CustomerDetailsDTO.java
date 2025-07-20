698c4d2281d796fff2dcb85596af02da0da62d0a
Ajay Gathadi <ajaygathadi@gmail.com>
2025-07-20 23:12:22 +0530

    Add DTO for customer details for the specific date

--- src/main/java/com/up2date/dto/CustomerDetailsDTO.java
+++ src/main/java/com/up2date/dto/CustomerDetailsDTO.java
@@ -1,13 +1,15 @@
 package com.up2date.dto;
 
-import java.time.LocalDate;
-
+/**
+ * Data Transfer Object for Customer Details.
+ * This DTO is used to transfer customer details including name, mobile number,
+ * total amount spent, services availed, and the name of the employee who assisted.
+ */
 public record CustomerDetailsDTO(
-        String customerName,
-        String mobileNumber,
-        LocalDate serviceTakenDate,
-        Long totalVisits,
-        Double totalAmount,
-        String services
+    String customerName,
+    String mobileNumber,
+    double totalAmount,
+    String services,
+    String employeeName
 ) {
 }
